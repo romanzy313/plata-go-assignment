f2c34e325c6cedd3ddcbb8358e0181cd8fb8a06e
Roman <romanvolovoy@gmail.com>
2026-08-23 22:48:24 +0200

    add echo web server

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/romanzy313/plata-go-assignment
 
 go 1.25.6
+
+require (
+	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/labstack/echo/v5 v5.3.1 // indirect
+)
--- main.go
+++ main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log/slog"
+	"os"
+
+	"github.com/joho/godotenv"
+	"github.com/labstack/echo/v5"
+)
 
 func main() {
-	fmt.Printf("%s\n", Handler())
+	err := godotenv.Load()
+	if err != nil {
+		slog.Error("Error loading .env file", "error", err)
+		os.Exit(1)
+	}
+
+	e := echo.New()
+	e.Logger = slog.Default()
+
+	e.GET("/health", func(c *echo.Context) error {
+		return c.String(200, "OK")
+	})
+
+	e.Start(fmt.Sprintf(":%s", os.Getenv("PORT")))
 }
